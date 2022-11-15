a8a18f407c68ab477003aa527a1c8fd7ab995c02
Gabriele <60697360+gabrielyyytte@users.noreply.github.com>
2022-11-15 22:00:34 +0200

    Update Helpers.py

--- Helpers.py
+++ Helpers.py
@@ -5,30 +5,33 @@ class Helpers:
 	def __init__(self):
 		pass
 
-	def resize(image, cnts, width=None, height=None, inter=cv2.INTER_AREA):
-			dim = None
-			(h, w) = image.shape[:2]
-			if width is None and height is None:
-				return image
-			if width is None:
-				r = height / float(h)
-				dim = (int(w * r), height)
-			else:
-				r = width / float(w)
-				dim = (width, int(h * r))
-			resized = cv2.resize(image, dim, interpolation=inter)
+	def resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+		dim = None
+		(h, w) = image.shape[:2]
+		if width is None and height is None:
+			return image
+		if width is None:
+			r = height / float(h)
+			dim = (int(w * r), height)
+		else:
+			r = width / float(w)
+			dim = (width, int(h * r))
+		resized = cv2.resize(image, dim, interpolation=inter)
+		
+		return resized
+	
+	def checkContours(cnts)
+		if len(cnts) == 2:
+			cnts = cnts[0]
+		elif len(cnts) == 3:
+			cnts = cnts[1]
+		else:
+			raise Exception('The length of the contour must be 2 or 3.')
 
-			if len(cnts) == 2:
-					cnts = cnts[0]
-			elif len(cnts) == 3:
-					cnts = cnts[1]
-			else:
-					raise Exception('The length of the contour must be 2 or 3.')
-
-			return resized, cnts
+		return cnts
 
 
-	def orders(image, pts):
+	def orders(pts):
 		rect = np.zeros((4, 2), dtype = "float32")
 		s = pts.sum(axis = 1)
 
@@ -38,6 +41,10 @@ class Helpers:
 		diff = np.diff(pts, axis = 1)
 		rect[1] = pts[np.argmin(diff)]
 		rect[3] = pts[np.argmax(diff)]
+		
+		return rect
+	
+	def transformation(image)
 
 		rect = Helpers.orders(pts)
 		(tl, tr, br, bl) = rect
@@ -59,5 +66,5 @@ class Helpers:
 		M = cv2.getPerspectiveTransform(rect, dst)
 		warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
-		return rect, warped
+		return warped
 
